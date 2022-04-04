e2493d72687f7f5cb24399ddb1a58fa48a94540a
Darshak Ranpariya <34344691+darshakpranpariya@users.noreply.github.com>
2022-04-04 11:26:28 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+
+# importing sys
+import sys
+# adding Folder_2 to the system path
+sys.path.insert(0, '../code/')
+import my_model as my_module
+import imp
+imp.reload(my_module)
+
+
+app = Flask(__name__)
+
+@app.route('/') 
+def index(): 
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    text = [x for x in request.form.values()]
+    prediction = my_module.generate_text_summary(text)
+
+    output = prediction
+
+    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
