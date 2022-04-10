48971b9839cd9fb42b9793087631079d201fc46d
Darshak Ranpariya <34344691+darshakpranpariya@users.noreply.github.com>
2022-04-10 14:04:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
 
     output = prediction
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Summarize text is here:- \n {}'.format(output))
 
 
 if __name__ == "__main__":
